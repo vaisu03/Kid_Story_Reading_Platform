3d798ee616c34d241d6df881cf68ab4fa59b9672
unknown <vaish6819@gmail.com>
2025-08-28 21:45:09 +0530

    Added full project

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Close database connection
+$conn->close();
+?>
